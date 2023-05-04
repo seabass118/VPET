c9c8fbdbc663a72c0c5015d864c0ed0c92548839
Seb Blackley <20054737@stu.mmu.ac.uk>
2023-05-04 22:24:45 +0100

    #3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import ViteExpress from "vite-express";
 
 const app = express();
+ViteExpress.config({ mode: "production" })
 
 app.get("/message", (_, res) => res.send("Hello from express!"));
 
